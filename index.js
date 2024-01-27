7062be0f5fa1b42a4c9d6e25446ac013b4fa7b37
Gamal Samadov <gamalsamadov@gmail.com>
2024-01-27 15:48:04 +0300

    env added

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors(
   {
-    origin: ["http://localhost:3000", process.env.CLIENTSIDE_URL],
+    origin: [process.env.CLIENTSIDE_URL],
     methods: ["POST", "GET", "PATCH", "DELETE"],
     credentials: true
   }
