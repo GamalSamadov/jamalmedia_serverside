d6cbde4e1fb0df758f53c7b478893ef6f6bf8133
Gamal Samadov <gamalsamadov@gmail.com>
2024-01-27 13:27:15 +0300

    Public folder fixing

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
-    origin: ["https://hsoubgram.vercel.app", "http://localhost:3000", "https://hsoubgram-87870483e74f.herokuapp.com"],
+    origin: ["https://hsoubgram.vercel.app", "http://localhost:3000", "https://hsoubgram-app-bf104640da73.herokuapp.com"],
     methods: ["POST", "GET", "PATCH", "DELETE"],
     credentials: true
   }
