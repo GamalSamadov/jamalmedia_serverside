bce6cb3c7cb9d1010eff2f2e41d730e4b1ef02a3
Gamal Samadov <gamalsamadov@gmail.com>
2024-01-29 14:43:19 +0300

    origin updated

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors(
   {
-    origin: [process.env.CLIENTSIDE_URL],
+    origin: [process.env.CLIENTSIDE_URL, "http://localhost:3000"],
     methods: ["POST", "GET", "PATCH", "DELETE"],
     credentials: true
   }
