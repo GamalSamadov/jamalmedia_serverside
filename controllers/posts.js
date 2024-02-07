46d817c95f63e606ba355b5dba0de85e748f19a5
Gamal Samadov <gamalsamadov@gmail.com>
2024-02-07 13:53:13 +0300

    get post / added

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,11 +3,11 @@ import User from "../models/User.js"
 
 export const getPostById = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     const post = await Post.findById(id)
-    res.status(200).json(post);
+    res.status(200).json(post)
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
 }
 
@@ -20,32 +20,32 @@ export const updatePost = async (req, res) => {
 
     const post = await Post.findOneAndUpdate({ _id: id }, { description: description }, { returnOriginal: false })
 
-    const userPosts = await Post.find({ userId: post.userId });
-    res.status(200).json(userPosts);
-    
+    const userPosts = await Post.find({ userId: post.userId })
+    res.status(200).json(userPosts)
+
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
 }
 
 export const deletePost = async (req, res) => {
   try {
-    
+
     const { id } = req.params
     await Post.deleteOne({ _id: id })
 
     res.status(200)
 
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
 }
 
 /* CREATE */
 export const createPost = async (req, res) => {
   try {
-    const { userId, description, picturePath } = req.body;
-    const user = await User.findById(userId);
+    const { userId, description, picturePath } = req.body
+    const user = await User.findById(userId)
     const newPost = new Post({
       userId,
       firstName: user.firstName,
@@ -56,58 +56,58 @@ export const createPost = async (req, res) => {
       picturePath,
       likes: {},
       comments: [],
-    });
-    await newPost.save();
+    })
+    await newPost.save()
 
-    const post = await Post.find();
-    res.status(201).json(post);
+    const post = await Post.find()
+    res.status(201).json(post)
   } catch (err) {
-    res.status(409).json({ message: err.message });
+    res.status(409).json({ message: err.message })
   }
-};
+}
 
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    const post = await Post.find()
+    res.status(200).json(post)
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
-};
+}
 
 export const getUserPosts = async (req, res) => {
   try {
-    const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
+    const { userId } = req.params
+    const post = await Post.find({ userId })
+    res.status(200).json(post)
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
-};
+}
 
 /* UPDATE */
 export const likePost = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { userId } = req.body;
-    const post = await Post.findById(id);
-    const isLiked = post.likes.get(userId);
+    const { id } = req.params
+    const { userId } = req.body
+    const post = await Post.findById(id)
+    const isLiked = post.likes.get(userId)
 
     if (isLiked) {
-      post.likes.delete(userId);
+      post.likes.delete(userId)
     } else {
-      post.likes.set(userId, true);
+      post.likes.set(userId, true)
     }
 
     const updatedPost = await Post.findByIdAndUpdate(
       id,
       { likes: post.likes },
       { new: true }
-    );
+    )
 
-    res.status(200).json(updatedPost);
+    res.status(200).json(updatedPost)
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message })
   }
-};
+}
--- routes/posts.js
+++ routes/posts.js
@@ -2,18 +2,18 @@ import express from "express"
 import { deletePost, getFeedPosts, getPostById, getUserPosts, likePost, updatePost } from "../controllers/posts.js"
 import { verifyToken } from "../middleware/auth.js"
 
-const router = express.Router();
+const router = express.Router()
 
 /* READ */
-router.get("/", getFeedPosts);
-router.get("/:userId/posts", getUserPosts);
-router.get(":id/", getPostById);
+router.get("/", getFeedPosts)
+router.get("/:userId/posts", getUserPosts)
+router.get("/:id/", getPostById)
 
 /* UPDATE */
-router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/like", verifyToken, likePost)
 router.patch("/:id", verifyToken, updatePost)
 
 // DELETE
 router.delete("/:id", verifyToken, deletePost)
 
-export default router;
+export default router
