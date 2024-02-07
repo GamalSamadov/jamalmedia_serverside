6617f44e3c4c1286ac34d3a89bc6394c9f081c9a
Gamal Samadov <gamalsamadov@gmail.com>
2024-02-07 13:13:05 +0300

    register invalid fields added

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,6 +38,10 @@ export const register = async (req, res) => {
       viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     })
+    if (!firstName || !lastName || !email || !password || !picturePath || !location || !occupation) {
+      res.status(400).json({ message: "Invalid fields" })
+      return
+    }
     const savedUser = await newUser.save()
     res.status(201).json(savedUser)
   } catch (err) {
